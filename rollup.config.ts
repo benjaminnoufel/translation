758060abe711d5e370f680f674ebd718dca028c8
Benjamin Noufel <benjamin.noufel@gmail.com>
2021-10-05 20:02:41 +0200

    :wrench: Adding commonjs and nodeResolve

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,3 +1,5 @@
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
 import remove from "rollup-plugin-delete";
 import {terser} from "rollup-plugin-terser";
 import typescript from "@rollup/plugin-typescript";
@@ -13,10 +15,13 @@ export default {
     plugins: [
         remove({targets: "dist/*"}),
         typescript(),
+        commonjs(),
+        nodeResolve(),
         terser()
     ],
     output: {
         file: "dist/translation.js",
-        format: "cjs"
+        format: "cjs",
+        sourcemap: true
     }
 };
